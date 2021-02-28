6ba2ea6c0e258a6cd9241f737949e5857f504172
zalwata <zalwata@hotmail.com>
2021-02-27 22:35:10 -0800

    BubbleSort runner is completed - SelectionSort, InsertionSort, MergeSort, QuickSort are required.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,2 +1,27 @@
 public class BubbleSort {
+
+    public void bubbleSort(int[] randomArray, int start, int end)
+    {
+        for(int lastUnsortedIndex = randomArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--)
+        {
+            for (int i = 0; i < lastUnsortedIndex; i++)
+            {
+                if(randomArray[i] > randomArray[i+1])
+                {
+                    swap(randomArray, i, i+1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public void swap(int[] array, int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
