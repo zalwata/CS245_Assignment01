7f1cf15afa9c8d8d581b58c9efb3cfb0a13b341a
zalwata <zalwata@hotmail.com>
2021-02-28 23:07:53 -0800

    AlgorithmValidityTester is completed/commented - InsertionSort, MergeSort, QuickSort are required.

--- src/AlgorithmValidityTester.java
+++ src/AlgorithmValidityTester.java
@@ -4,16 +4,20 @@ public class AlgorithmValidityTester {
 
     public static int ARRAYS_LENGTHS = 5;
 
-    //record run time for bubble sort
-    public static void bubbleSortValidityTesting(int[] list)
+    /**
+     * runs bubble sort algorithm with the same testing array with random values
+     * to make sure this sorting algorithm produce same output as other algorithms
+     * @void
+     */
+    public static void bubbleSortValidityTesting(int[] testingArray)
     {
-        BubbleSort sorter = new BubbleSort();
-        sorter.bubbleSort(list);
+        BubbleSort sortingAlgorithmObj = new BubbleSort();
+        sortingAlgorithmObj.bubbleSort(testingArray);
         System.out.println("random array after sorting-----------------------------------------------------BUBBLESORT");
         System.out.print("[");
         for(int i=0;i < ARRAYS_LENGTHS;i++)
         {
-            System.out.print(list[i]);
+            System.out.print(testingArray[i]);
             if(i != (ARRAYS_LENGTHS - 1))
             {
                 System.out.print(", ");
@@ -22,16 +26,20 @@ public class AlgorithmValidityTester {
         System.out.println("]");
     }
 
-    //record run time for insertion sort
-    public static void insertionSortValidityTesting(int[] list)
+    /**
+     * runs insertion sort algorithm with the same testing array with random values
+     * to make sure this sorting algorithm produce same output as other algorithms
+     * @void
+     */
+    public static void insertionSortValidityTesting(int[] testingArray)
     {
-        InsertionSort sorter = new InsertionSort();
-        sorter.insertionSort(list);
+        InsertionSort sortingAlgorithmObj = new InsertionSort();
+        sortingAlgorithmObj.insertionSort(testingArray);
         System.out.println("random array after sorting--------------------------------------------------INSERTIONSORT");
         System.out.print("[");
         for(int i=0;i < ARRAYS_LENGTHS;i++)
         {
-            System.out.print(list[i]);
+            System.out.print(testingArray[i]);
             if(i != (ARRAYS_LENGTHS - 1))
             {
                 System.out.print(", ");
@@ -40,16 +48,20 @@ public class AlgorithmValidityTester {
         System.out.println("]");
     }
 
-    //record run time for selection sort
-    public static void selectionSortValidityTesting(int[] list)
+    /**
+     * runs selection sort algorithm with the same testing array with random values
+     * to make sure this sorting algorithm produce same output as other algorithms
+     * @void
+     */
+    public static void selectionSortValidityTesting(int[] testingArray)
     {
-        SelectionSort sorter = new SelectionSort();
-        sorter.selectionSort(list);
+        SelectionSort sortingAlgorithmObj = new SelectionSort();
+        sortingAlgorithmObj.selectionSort(testingArray);
         System.out.println("random array after sorting--------------------------------------------------SELECTIONSORT");
         System.out.print("[");
         for(int i=0;i < ARRAYS_LENGTHS;i++)
         {
-            System.out.print(list[i]);
+            System.out.print(testingArray[i]);
             if(i != (ARRAYS_LENGTHS - 1))
             {
                 System.out.print(", ");
@@ -58,16 +70,20 @@ public class AlgorithmValidityTester {
         System.out.println("]");
     }
 
-    //record run time for merge sort
-    public static void mergeSortValidityTesting(int[] list)
+    /**
+     * runs merge sort algorithm with the same testing array with random values
+     * to make sure this sorting algorithm produce same output as other algorithms
+     * @void
+     */
+    public static void mergeSortValidityTesting(int[] testingArray)
     {
-        MergeSort sorter = new MergeSort();
-        sorter.mergeSort(list);
+        MergeSort sortingAlgorithmObj = new MergeSort();
+        sortingAlgorithmObj.mergeSort(testingArray);
         System.out.println("random array after sorting------------------------------------------------------MERGESORT");
         System.out.print("[");
         for(int i=0;i < ARRAYS_LENGTHS;i++)
         {
-            System.out.print(list[i]);
+            System.out.print(testingArray[i]);
             if(i != (ARRAYS_LENGTHS - 1))
             {
                 System.out.print(", ");
@@ -76,16 +92,20 @@ public class AlgorithmValidityTester {
         System.out.println("]");
     }
 
-    //record run time for quick sort
-    public static void quickSortValidityTesting(int[] list)
+    /**
+     * runs quick sort algorithm with the same testing array with random values
+     * to make sure this sorting algorithm produce same output as other algorithms
+     * @void
+     */
+    public static void quickSortValidityTesting(int[] testingArray)
     {
-        QuickSort sorter = new QuickSort();
-        sorter.quickSort(list);
+        QuickSort sortingAlgorithmObj = new QuickSort();
+        sortingAlgorithmObj.quickSort(testingArray);
         System.out.println("random array after sorting------------------------------------------------------QUICKSORT");
         System.out.print("[");
         for(int i=0;i < ARRAYS_LENGTHS;i++)
         {
-            System.out.print(list[i]);
+            System.out.print(testingArray[i]);
             if(i != (ARRAYS_LENGTHS - 1))
             {
                 System.out.print(", ");
@@ -97,22 +117,21 @@ public class AlgorithmValidityTester {
     public static void main(String[] args) {
 
         //generate array with random values
-        int listsize = ARRAYS_LENGTHS;
-        int[] list = new int[listsize];
-        Random randomGenerator = new Random();
-        for(int i=0;i < listsize;i++)
+        int[] testingArray = new int[ARRAYS_LENGTHS];
+        Random randomValueGenerator = new Random();
+        for(int i=0;i < ARRAYS_LENGTHS;i++)
         {
-            list[i] = randomGenerator.nextInt();
+            testingArray[i] = randomValueGenerator.nextInt();
         }
 
-        System.out.println("array size: " + ARRAYS_LENGTHS);
+        System.out.println("array size: " + AlgorithmValidityTester.ARRAYS_LENGTHS);
         //print the random array before sorting
         System.out.println("random array before sorting--------------------------------------------------------------");
         System.out.print("[");
-        for(int i=0;i < listsize;i++)
+        for(int i=0;i < ARRAYS_LENGTHS;i++)
         {
-            System.out.print(list[i]);
-            if(i != (listsize - 1))
+            System.out.print(testingArray[i]);
+            if(i != (ARRAYS_LENGTHS - 1))
             {
                 System.out.print(", ");
             }
@@ -120,20 +139,17 @@ public class AlgorithmValidityTester {
         System.out.println("]");
 
         //run time analysis on each sorting algorithm
-        int[] listClone1 = list.clone();
-        int[] listClone2 = list.clone();
-        int[] listClone3 = list.clone();
-        int[] listClone4 = list.clone();
-        int[] listClone5 = list.clone();
+        int[] listClone1 = testingArray.clone();
+        int[] listClone2 = testingArray.clone();
+        int[] listClone3 = testingArray.clone();
+        int[] listClone4 = testingArray.clone();
+        int[] listClone5 = testingArray.clone();
 
         bubbleSortValidityTesting(listClone1);
         insertionSortValidityTesting(listClone2);
         selectionSortValidityTesting(listClone3);
         mergeSortValidityTesting(listClone4);
         quickSortValidityTesting(listClone5);
-
-
-
     }
 
 }
