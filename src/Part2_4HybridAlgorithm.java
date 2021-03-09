36f08bf93f113696f7addc9c9144a9da50a5e89c
zalwata <zalwata@hotmail.com>
2021-03-08 20:23:39 -0800

    part 2.3 modified quadratic sort is completed. changing data type to doulbe is required.

---
+++ src/Part2_4HybridAlgorithm.java
@@ -0,0 +1,17 @@
+/**
+ * Hybrid algorithm
+ * - calls randomised quicksort on large arrays
+ * - calls quadratic sort for smaller arrays
+ * - make implementation choice based on the length of the sub-array being sorted
+ *
+ * idea 1:
+ * -> based on the experimental data, randomized quicksort performs O(n lg n) on large arrays.
+ * -> need data for justification to see how randomized quicksort and quadratic sort performs on different sizes.
+ *
+ */
+
+public class Part2_4HybridAlgorithm {
+
+
+
+}
